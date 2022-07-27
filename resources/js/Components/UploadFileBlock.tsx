089f67e12eac31b098f85bb3399759e73f2569f7
H1ako <nikita-sobolev-wd@yandex.ru>
2022-07-27 15:54:25 +0400

    made file upload by drag it

--- resources/js/Components/UploadFileBlock.tsx
+++ resources/js/Components/UploadFileBlock.tsx
@@ -6,15 +6,43 @@ import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function UploadFileBlock() {
-    const [file, setFile] = React.useState<File | null | undefined>(null)
+    const [ files, setFiles ] = React.useState<FileList | []>([])
+    const [ isDragging, setIsDragging ] = React.useState<boolean>(false)
+
+    const uploadHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const uploadedFiles: any = e.target?.files
+
+        setFiles(uploadedFiles)
+    }
+
+    const dropFileHandler = (e: React.DragEvent): void => {
+        e.stopPropagation();
+        e.preventDefault()
+
+        const uploadedFiles: any = e.dataTransfer.files
+
+        setFiles(uploadedFiles)
+        setIsDragging(false)
+    }
+
+    const dragOverHandler = (e: React.DragEvent) => {
+        e.stopPropagation()
+        e.preventDefault()
+    }
 
     return (
-        <div className="upload-container">
+        <div
+            onDrop={dropFileHandler}
+            onDragOver={dragOverHandler}
+            onDragEnter={() => setIsDragging(true)}
+            onDragLeave={() => setIsDragging(false)}
+            className={`upload-container${isDragging ? ' active' : ''}`}
+        >
             <label className="upload-file-block" htmlFor='upload-file-block'>
                 <FontAwesomeIcon className='upload-file-block__icon' icon={faFileArrowDown} />
                 <h1>Press or Hover the File to Upload</h1>
             </label>
-            <input type="file" id='upload-file-block' onChange={e => setFile(e.target?.files?.[0])} />
+            <input type="file" multiple id='upload-file-block' onChange={uploadHandler} />
         </div>
     )
 }
--- resources/js/_types.tsx
+++ resources/js/_types.tsx
@@ -1,4 +1,4 @@
-type FileType = 'video' | 'image' | 'archive' | 'other'
+type FileType = 'video' | 'image' | 'archive' | 'text' | 'other'
 type RequestStatusType = 'idle' | 'loading' | 'failed' | 'succeeded'
 type SpaceSizeType = number
 type IdType = number | string
