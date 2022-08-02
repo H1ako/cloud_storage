26cc85a66df601ede50b8b270ab17cca04dd2902
H1ako <nikita-sobolev-wd@yandex.ru>
2022-08-02 14:02:06 +0400

    fixed type bug

--- resources/js/Layouts/ClickOutsideLayout.tsx
+++ resources/js/Layouts/ClickOutsideLayout.tsx
@@ -4,14 +4,17 @@ import React from 'react'
 
 interface Props {
     children: React.ReactElement,
-    onClick: (e: MouseEvent) => void
+    onClick: (e: MouseEvent) => void,
 }
 
 export const ClickOutsideLayout = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
+    
     const handleClickOutside = (e: MouseEvent) => {
-        if (!ref?.current) return
+        const refFixed = ref as React.RefObject<HTMLDivElement>
 
-        if (props.onClick && !ref.current.contains(e.target)) {
+        if (!(refFixed.current)) return
+
+        if (props.onClick && !refFixed.current.contains(e.target as HTMLElement)) {
             props.onClick(e)
         }
     }
