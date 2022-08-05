d3d8420c4c1777fd36df30a700a8f423ffd5e449
H1ako <nikita-sobolev-wd@yandex.ru>
2022-08-05 21:14:07 +0400

    made file page

--- resources/js/Pages/FilePage.tsx
+++ resources/js/Pages/FilePage.tsx
@@ -6,6 +6,10 @@ import PageLayout from '../Layouts/PageLayout';
 import FileBgByType from '../Components/FileBgByType';
 // store
 import { useAppDispatch } from '../store/hooks';
+import useFileApi from '../libs/useFileApi';
+// icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 
 interface Props {
@@ -15,10 +19,27 @@ interface Props {
 
 export default function FilePage({user, file}: Props) {
     const dispatch = useAppDispatch()
+    const fileApi = useFileApi(file, dispatch)
 
     return (
         <PageLayout user={user}>
-            <FileBgByType file={file} className='file-container' />
+            <div className="file-controll-btns">
+                <button className="file-controll-btns__btn">Rename</button>
+                <button className="file-controll-btns__btn">Delete</button>
+                <button className="file-controll-btns__btn">Full Size</button>
+            </div>
+            <div className="file-info">
+                <FileBgByType file={file} className='file-info__bg' />
+                <h1 className="file-info__name">{file.name}</h1>
+                <h3 className="file-info__size">{file.size}</h3>
+            </div>
+            <div className="main-btns">
+                <button className="main-btns__btn">Stop Sharing</button>
+                <a href={file.path} download={file.name}>
+                    <FontAwesomeIcon icon={faFileArrowDown} />
+                    Download
+                </a>
+            </div>
         </PageLayout>
     )
 }
