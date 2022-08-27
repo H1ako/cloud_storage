4b0b49b92c8a01fd40f6123b4bfa458e6af8ff65
H1ako <nikita-sobolev-wd@yandex.ru>
2022-08-28 01:25:08 +0400

    updated size displaying method

--- app/Services/SizeService.php
+++ app/Services/SizeService.php
@@ -5,18 +5,17 @@ namespace App\Services;
 class SizeService
 {
     public function getSize(int $originalSize) {
-        $type = 'Kb';
-        $size = $originalSize / 1024;
-        if ($size > 1024) {
-            $size /= 1024;
-            $type = 'Mb';
-        }
-        if ($size > 1024) {
-            $size /= 1024;
-            $type = 'Gb';
-        }
-        $roundedSize = round($size, 2);
-        
-        return "{$roundedSize}{$type}";
+        // $S = 'KMGTPEZY';
+        // $F = floor((strlen($originalSize) - 1) / 3);
+        // return sprintf("%.2f", $originalSize/pow(1024, $F)).' '.@$S[$F-1].'B';
+
+        $units = ['B', 'KB', 'MB', 'GB', 'TB']; 
+
+        $bytes = max($originalSize, 0); 
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
+        $pow = min($pow, count($units) - 1); 
+        $bytes /= pow(1024, $pow);
+
+        return round($bytes, 2) . ' ' . $units[$pow]; 
     }
 }
